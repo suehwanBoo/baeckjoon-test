913252ab2b4f2a279127f1dcc2ae45d99dee216e
suehwanBoo <rndjdieo119@naver.com>
2023-04-12 17:28:56 +0900

    first commit

---
+++ src/baekjoon_git/baekjoon_1000.java
@@ -0,0 +1,15 @@
+package baekjoon_git;
+
+import java.util.Scanner;
+public class baekjoon_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+
+        a=sc.nextInt();
+        b=sc.nextInt();
+
+        System.out.println(a+b);
+    }
+}
